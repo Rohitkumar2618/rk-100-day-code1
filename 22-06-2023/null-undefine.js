56c00a2d927b7776c996555e2f0d6a1972f7f236
Rohitkumar2618 <109166754+Rohitkumar2618@users.noreply.github.com>
2023-06-23 21:38:52 +0530

    Add files via upload

---
+++ 22-06-2023/null-undefine.js
@@ -0,0 +1,26 @@
+
+// In a such a way we can define a null value
+
+// null
+const age = null;
+console.log(age);
+
+// but what if we want to change/ update the value of age 
+//  so for that we use let as a data type
+
+let age1 = 3;  // we use age1 because we can not assign  same name 
+console.log(age1);
+
+// undefine
+
+let x= undefined;
+console.log(x);
+
+
+// another way to undefine the value
+
+let  x1;
+console.log(x);
+
+console.log(typeof null);
+// its give output object because its a official bug of js 
---
+++ 22-06-2023/object.js
@@ -0,0 +1,24 @@
+// object
+
+// in another that type we can store only one value 4
+
+// but in object we can put the multiple values
+
+// object
+
+const person = {
+    name:'john',
+    age: 25,
+}
+
+console.log(person);
+
+// if you want the perticular element in object so we use dot
+
+console.log(person.age); 
+
+// below line show the type of person that is objet
+console.log( typeof person);
+
+// but we 
+console.log( typeof person.name);
